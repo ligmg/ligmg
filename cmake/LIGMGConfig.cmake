c2b5f4f8da7cd7e8bc3977cb97b53a4b1fa75177
Tristan Konolige <tristan.konolige@gmail.com>
2020-07-07 11:24:43 -0700

    Propagate cmake dependencies downstream

--- cmake/LIGMGConfig.cmake
+++ cmake/LIGMGConfig.cmake
@@ -1,6 +1,19 @@
+set(CALLERS_CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH})
+get_filename_component(CURRENT_CONFIG_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+set(CMAKE_MODULE_PATH "${CURRENT_CONFIG_DIR};${CMAKE_MODULE_PATH}")
+
+# CMake doesn't export interface directories of a target. This is a hack to
+# make these dependencies available to downstream users.
+set(mxx_DIR @mxx_DIR@)
+set(CombBLAS_DIR @CombBLAS_DIR@)
+set(PETSC_DIR @PETSC_DIR@)
+set(PETSC_ARCH @PETSC_ARCH@)
+
 find_package(MPI REQUIRED QUIET)
 find_package(mxx REQUIRED QUIET)
 find_package(CombBLAS REQUIRED QUIET)
 find_package(PETSc REQUIRED QUIET)
 
-include("${CMAKE_CURRENT_LIST_DIR}/LIGMGTargets.cmake}")
+set(CMAKE_MODULE_PATH ${CALLERS_CMAKE_MODULE_PATH})
+
+include("${CMAKE_CURRENT_LIST_DIR}/LIGMGTargets.cmake")
